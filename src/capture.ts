988bc76189fb6b06c229d3956c4f1ca8dddb030c
bouzuya <m@bouzuya.net>
2018-01-05 23:48:13 +0900

    add scenario2

--- src/capture.ts
+++ src/capture.ts
@@ -9,13 +9,11 @@ const ensureKey = (tmpl: string, key: string): string => {
     : tmpl + '/{' + key + '}';
 };
 
-const capture = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
+const scenario1 = async (page: puppeteer.Page) => {
+  const capturedDirectory = '.tmp/captured/';
   const name = 'scenario1';
   const language = 'ja';
-  const url = 'http://example.com';
+  const url = 'https://blog.bouzuya.net/2017/01/01/';
   const viewport = '320x480';
 
   // language
@@ -37,13 +35,57 @@ const capture = async () => {
   await page.goto(url);
 
   // screenshot
-  const capturedDirectory = '.tmp/captured/';
   const type = 'png';
   const path = pathJoin(capturedDirectory, name2 + '.' + type);
   const dir = getDirectoryName(path);
   await fs.ensureDir(dir);
   await page.screenshot({ path, type });
+};
 
+const scenario2 = async (page: puppeteer.Page) => {
+  const capturedDirectory = '.tmp/captured/';
+  const name = 'scenario2';
+  const language = 'ja';
+  const url = 'https://blog.bouzuya.net/2017/01/02/';
+  const viewport = '320x480';
+
+  // language
+  await page.evaluateOnNewDocument([
+    'Object.defineProperty(navigator, "language", {',
+    '  get() { return "' + language + '"; }',
+    '})'
+  ].join(''));
+  const name1 = format(ensureKey(name, 'language'), 'language', language);
+
+  // viewport
+  const [width, height] = viewport
+    .split('x')
+    .map((s) => parseInt(s, 10));
+  const viewportObject = { height, width };
+  page.setViewport(viewportObject);
+  const name2 = format(ensureKey(name1, 'viewport'), 'viewport', viewport);
+
+  await page.goto(url);
+
+  // screenshot
+  const type = 'png';
+  const path = pathJoin(capturedDirectory, name2 + '.' + type);
+  const dir = getDirectoryName(path);
+  await fs.ensureDir(dir);
+  await page.screenshot({ path, type });
+};
+
+const capture = async () => {
+  const browser = await puppeteer.launch();
+  const scenarios = [
+    scenario1,
+    scenario2
+  ];
+  await scenarios.reduce((promise, scenario) => {
+    return promise
+      .then(() => browser.newPage())
+      .then((page) => scenario(page));
+  }, Promise.resolve());
   await browser.close();
 };
 
