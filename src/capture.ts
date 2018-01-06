b30a237efbd68332df8cb79c75375a6a51e10905
bouzuya <m@bouzuya.net>
2018-01-07 00:06:51 +0900

    inline screenshotHelper

--- src/capture.ts
+++ src/capture.ts
@@ -3,28 +3,19 @@ import { dirname as getDirectoryName, join as pathJoin } from 'path';
 import * as puppeteer from 'puppeteer';
 import { Options } from './data/options';
 
-const screenshotHelper = async (
-  page: puppeteer.Page,
-  name: string,
-  capturedDirectory: string,
-  clip?: puppeteer.BoundingBox
-): Promise<void> => {
-  const type = 'png';
-  const path = pathJoin(capturedDirectory, name + '.' + type);
-  const dir = getDirectoryName(path);
-  await fs.ensureDir(dir);
-  await page.screenshot({ clip, path, type });
-};
-
 const capture = async ({ path: { captured }, scenarios }: Options) => {
   const browser = await puppeteer.launch();
   await scenarios.reduce((promise, { action, name }) => {
-    return promise
-      .then(() => browser.newPage())
-      .then((page) => action(page).then((o) => Object.assign({ page }, o)))
-      .then(({ page, clip }) => {
-        return screenshotHelper(page, name, captured, clip);
-      });
+    return promise.then(async () => {
+      const page = await browser.newPage();
+      const screenshotOptions = await action(page);
+      const { clip } = screenshotOptions;
+      const type = 'png';
+      const path = pathJoin(captured, name + '.' + type);
+      const dir = getDirectoryName(path);
+      await fs.ensureDir(dir);
+      await page.screenshot({ clip, path, type });
+    });
   }, Promise.resolve());
   await browser.close();
 };
