f100877fa89f671c6051d84551776f2cb0429e1e
bouzuya <m@bouzuya.net>
2018-01-06 21:37:10 +0900

    extract some helpers

--- src/capture.ts
+++ src/capture.ts
@@ -9,37 +9,59 @@ const ensureKey = (tmpl: string, key: string): string => {
     : tmpl + '/{' + key + '}';
 };
 
-const scenario1 = async (page: puppeteer.Page) => {
-  const capturedDirectory = '.tmp/captured/';
-  const name = 'scenario1';
-  const language = 'ja';
-  const url = 'https://blog.bouzuya.net/2017/01/01/';
-  const viewport = '320x480';
-
-  // language
+const languageHelper = async (
+  page: puppeteer.Page,
+  name: string,
+  language: string
+): Promise<string> => {
   await page.evaluateOnNewDocument([
     'Object.defineProperty(navigator, "language", {',
     '  get() { return "' + language + '"; }',
     '})'
   ].join(''));
-  const name1 = format(ensureKey(name, 'language'), 'language', language);
+  return format(ensureKey(name, 'language'), 'language', language);
+};
 
+const screenshotHelper = async (
+  page: puppeteer.Page,
+  name: string,
+  capturedDirectory: string,
+  clip?: puppeteer.BoundingBox
+): Promise<void> => {
+  const type = 'png';
+  const path = pathJoin(capturedDirectory, name + '.' + type);
+  const dir = getDirectoryName(path);
+  await fs.ensureDir(dir);
+  await page.screenshot({ clip, path, type });
+};
+
+const viewportHelper = async (
+  page: puppeteer.Page,
+  name: string,
+  viewport: string
+): Promise<string> => {
   // viewport
   const [width, height] = viewport
     .split('x')
     .map((s) => parseInt(s, 10));
   const viewportObject = { height, width };
   page.setViewport(viewportObject);
-  const name2 = format(ensureKey(name1, 'viewport'), 'viewport', viewport);
+  return format(ensureKey(name, 'viewport'), 'viewport', viewport);
+};
+
+const scenario1 = async (page: puppeteer.Page): Promise<void> => {
+  const capturedDirectory = '.tmp/captured/';
+  const name = 'scenario1';
+  const language = 'ja';
+  const url = 'https://blog.bouzuya.net/2017/01/01/';
+  const viewport = '320x480';
+
+  const name1 = await languageHelper(page, name, language);
+  const name2 = await viewportHelper(page, name1, viewport);
 
   await page.goto(url);
 
-  // screenshot
-  const type = 'png';
-  const path = pathJoin(capturedDirectory, name2 + '.' + type);
-  const dir = getDirectoryName(path);
-  await fs.ensureDir(dir);
-  await page.screenshot({ path, type });
+  await screenshotHelper(page, name2, capturedDirectory);
 };
 
 const scenario2 = async (page: puppeteer.Page) => {
@@ -49,30 +71,12 @@ const scenario2 = async (page: puppeteer.Page) => {
   const url = 'https://blog.bouzuya.net/2017/01/02/';
   const viewport = '320x480';
 
-  // language
-  await page.evaluateOnNewDocument([
-    'Object.defineProperty(navigator, "language", {',
-    '  get() { return "' + language + '"; }',
-    '})'
-  ].join(''));
-  const name1 = format(ensureKey(name, 'language'), 'language', language);
-
-  // viewport
-  const [width, height] = viewport
-    .split('x')
-    .map((s) => parseInt(s, 10));
-  const viewportObject = { height, width };
-  page.setViewport(viewportObject);
-  const name2 = format(ensureKey(name1, 'viewport'), 'viewport', viewport);
+  const name1 = await languageHelper(page, name, language);
+  const name2 = await viewportHelper(page, name1, viewport);
 
   await page.goto(url);
 
-  // screenshot
-  const type = 'png';
-  const path = pathJoin(capturedDirectory, name2 + '.' + type);
-  const dir = getDirectoryName(path);
-  await fs.ensureDir(dir);
-  await page.screenshot({ path, type });
+  await screenshotHelper(page, name2, capturedDirectory);
 };
 
 const capture = async () => {
