6a7ef067157bf90e5a640a4dec14d0549896b661
bouzuya <m@bouzuya.net>
2018-01-07 14:07:10 +0900

    fix type

--- src/approve.ts
+++ src/approve.ts
@@ -1,7 +1,9 @@
 import * as fs from 'fs-extra';
 import { Options } from './data/options';
 
-const approve = async ({ path: { approved, captured } }: Options) => {
+const approve = async (
+  { path: { approved, captured } }: Options
+): Promise<void> => {
   await fs.copy(captured, approved, {
     overwrite: true,
     recursive: true
